c8d917bab3b3b43f04e69009da53185364d90e96
mdsagormia1151 <mdsagormia1151@gmail.com>
2026-04-30 16:32:00 +0600

    Static Header and Footer Added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+import { FaYoutube, FaFacebook, FaXTwitter } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#244D3F] text-white p-6">
+      <div className="max-w-7xl mx-auto text-center">
+        {/* Brand Name */}
+        <h1 className="text-5xl font-bold">
+          Keen<span className="font-light">Keeper</span>
+        </h1>
+        <p className="mt-2 text-sm md:text-base max-w-xl mx-auto text-white opacity-80">
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </p>
+        <h3 className="text-xl my-4">Social Links</h3>
+        {/* Social Links */}
+        <div className="flex justify-center gap-6">
+          <a
+            href="#"
+            className="p-3 rounded-full bg-white text-[#244D3F] hover:bg-[#1a886165] hover:text-white transition-colors duration-300"
+          >
+            <FaYoutube size={20} />
+          </a>
+          <a
+            href="#"
+            className="p-3 rounded-full bg-white text-[#244D3F] hover:bg-[#1a886165] hover:text-white transition-colors duration-300"
+          >
+            <FaFacebook size={20} />
+          </a>
+          <a
+            href="#"
+            className="p-3 rounded-full bg-white text-[#244D3F] hover:bg-[#1a886165] hover:text-white transition-colors duration-300"
+          >
+            <FaXTwitter size={20} />
+          </a>
+        </div>
+
+        {/* Bottom Links */}
+        <div className="mt-8 flex flex-col md:flex-row justify-between items-center text-sm border-t border-[#1a886165] pt-6">
+          <p>© 2026 KeenKeeper. All rights reserved.</p>
+          <div className="flex gap-6 mt-4 md:mt-0">
+            <a href="#" className="hover:underline">
+              Privacy Policy
+            </a>
+            <a href="#" className="hover:underline">
+              Terms of Service
+            </a>
+            <a href="#" className="hover:underline">
+              Cookies
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,91 @@
+// components/Navbar.jsx
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { BiHomeAlt2 } from "react-icons/bi";
+import { FaRegClock, FaChartLine } from "react-icons/fa6";
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const navItems = [
+    { name: "Home", href: "/", icon: <BiHomeAlt2 className="text-xl" /> },
+    {
+      name: "Timeline",
+      href: "/timeline",
+      icon: <FaRegClock className="text-xl" />,
+    },
+    {
+      name: "Stats",
+      href: "/stats",
+      icon: <FaChartLine className="text-xl" />,
+    },
+  ];
+
+  return (
+    <nav className="bg-white border-b border-[#E9E9E9]">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex h-15 items-center justify-between">
+          {/* Logo */}
+          <div className="text-[#244d3f] font-bold text-xl">
+            Keen<span className="font-semibold">Keeper</span>
+          </div>
+
+          {/* Desktop Menu */}
+          <div className="hidden md:flex space-x-2">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className={`flex items-center gap-1 px-3 py-2 rounded-md text-sm font-medium transition-colors
+                  ${
+                    pathname === item.href
+                      ? "bg-[#244D3F] text-white"
+                      : "text-[#64748B] hover:text-white hover:bg-[#244D3F]"
+                  }`}
+              >
+                {item.icon}
+                {item.name}
+              </Link>
+            ))}
+          </div>
+
+          {/* Mobile Hamburger Button */}
+          <div className="md:hidden">
+            <button
+              onClick={() => setMenuOpen(!menuOpen)}
+              className="text-[#244D3F] focus:outline-none"
+            >
+              {menuOpen ? <HiX size={28} /> : <HiMenu size={28} />}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile Dropdown Menu */}
+      {menuOpen && (
+        <div className="md:hidden bg-white border-t border-[#E9E9E9] px-4 pb-4 space-y-2">
+          {navItems.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              onClick={() => setMenuOpen(false)}
+              className={`flex items-center justify-center text-left gap-2 px-3 py-2 rounded-md text-sm font-medium transition-colors
+                ${
+                  pathname === item.href
+                    ? "bg-[#244D3F] text-white"
+                    : "text-[#64748B] hover:text-white hover:bg-[#244D3F]"
+                }`}
+            >
+              {item.icon}
+              {item.name}
+            </Link>
+          ))}
+        </div>
+      )}
+    </nav>
+  );
+}
