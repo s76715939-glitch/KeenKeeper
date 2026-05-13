6478da5f3e46539ce289a4ed5587da752a18f1a0
mdsagormia1151 <mdsagormia1151@gmail.com>
2026-05-13 12:54:10 +0600

    Final check and bug fixed

--- src/components/FriendsSection/FriendsSection.jsx
+++ src/components/FriendsSection/FriendsSection.jsx
@@ -1,28 +1,30 @@
-"use client";
+"use client"
 import { useEffect, useState } from "react";
-import FriendCard from "../FriendCard/FriendCard";
+import FriendCard from '../FriendCard/FriendCard';
 import Loading from "../Loading/Loading";
 const FriendsSection = () => {
   const [friends, setFriends] = useState([]);
   useEffect(() => {
-    fetch("/FriendsData.json", { cache: "no-store" })
+    fetch("/FriendsData.json")
       .then((res) => res.json())
       .then((data) => setFriends(data));
   }, []);
 
-  return (
-    <div className="container mx-auto p-10">
-      <h3 className="text-[#1F2937] text-2xl font-semibold py-2.5">
-        Your Friends
-      </h3>
-      {friends.length === 0 && <Loading />}
-      <div className="bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
-        {friends.map((f) => (
-          <FriendCard key={f.id} f={f} />
-        ))}
+    return (
+        <div className="container mx-auto p-10">
+            <h3 className="text-[#1F2937] text-2xl font-semibold py-2.5">Your Friends</h3>
+            {
+                friends.length === 0 && <Loading/>
+            }
+            <div className="bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-full">
+                {
+                    friends.map(f => (
+                        <FriendCard key={f.id} f={f} />
+                    ))
+                }
+            </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default FriendsSection;
